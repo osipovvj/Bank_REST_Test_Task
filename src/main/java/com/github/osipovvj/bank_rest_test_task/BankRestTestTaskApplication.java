a884f2b20d2ca2da2be724f65974af0882e77fdb
osivaM <o.v.v.9322@gmail.com>
2025-05-29 17:08:30 +0500

    init commit

---
+++ src/main/java/com/github/osipovvj/bank_rest_test_task/BankRestTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.github.osipovvj.bank_rest_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankRestTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankRestTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/osipovvj/bank_rest_test_task/BankRestTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.osipovvj.bank_rest_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRestTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

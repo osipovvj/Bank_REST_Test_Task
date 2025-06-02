e557307e33814573fa3ac388b548ae24e8562b2f
osivaM <o.v.v.9322@gmail.com>
2025-06-02 19:33:13 +0500

    fix: remove unused method findAllByOwnerId() in CardRepository

--- src/main/java/com/github/osipovvj/bank_rest_test_task/repository/CardRepository.java
+++ src/main/java/com/github/osipovvj/bank_rest_test_task/repository/CardRepository.java
@@ -1,9 +1,6 @@
 package com.github.osipovvj.bank_rest_test_task.repository;
 
 import com.github.osipovvj.bank_rest_test_task.entity.Card;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -13,5 +10,4 @@ import java.util.UUID;
 public interface CardRepository extends JpaRepository<Card, UUID>, JpaSpecificationExecutor<Card> {
     boolean existsByCardNumber(String cardNumber);
     Optional<Card> findByIdAndOwnerId(UUID id, UUID ownerId);
-    Page<Card> findAllByOwnerId(UUID ownerId, Specification<Card> spec, Pageable pageable);
 }
